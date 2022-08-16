fd06e21b5a449754cc67841aa4132511eb7f9d03
ChanwooAn <55792553+ChanwooAn@users.noreply.github.com>
2022-08-17 04:29:28 +0900

    Email+pw Login

--- app/src/main/java/com/example/datingapppractice/auth/LoginActivity.kt
+++ app/src/main/java/com/example/datingapppractice/auth/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.example.datingapppractice.auth
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
 import android.util.Log
 import android.view.MotionEvent
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.datingapppractice.MainActivity
 import com.example.datingapppractice.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -29,11 +32,20 @@ class LoginActivity : AppCompatActivity() {
         auth= Firebase.auth
 
 
-        /*login_pwSeeBtn.setOnTouchListener { _, motionEvent ->
-            when(motionEvent){
-               MotionEvent.ACTION_DOWN->
+        login_pwSeeBtn.setOnTouchListener { _, motionEvent ->
+            when(motionEvent.action){
+                MotionEvent.ACTION_DOWN -> login_pw.inputType=InputType.TYPE_CLASS_TEXT
+                MotionEvent.ACTION_MOVE -> {login_pw.inputType=InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD}
+                MotionEvent.ACTION_UP -> {login_pw.inputType=InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD}
+                MotionEvent.ACTION_CANCEL -> {login_pw.inputType=InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD}
+
             }
-        }*/
+
+            true
+        }
+
+
+
         login_loginBtn.setOnClickListener{
             auth.signInWithEmailAndPassword(login_id.text.toString(),login_pw.text.toString() )
                 .addOnCompleteListener(this) { task ->
@@ -42,7 +54,8 @@ class LoginActivity : AppCompatActivity() {
                         Log.d(TAG, "signInWithEmail:success")
                         val user = auth.currentUser
 
-                        startActivity(Intent(this,MainActivity::class.java))
+                        startActivity(Intent(this,MainActivity::class.java).
+                        setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK ))
                         //updateUI(user)
                     } else {
                         // If sign in fails, display a message to the user.
